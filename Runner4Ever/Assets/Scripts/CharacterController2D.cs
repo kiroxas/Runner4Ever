69a68bc081b23d2e7a592f0e1255df976563a3d3
Mouton Guillaume <mouton.guillaume88@gmail.com>
2017-06-20 11:41:04 +0200

    can assign action to input in character controller via unity editor

--- Runner4Ever/Assets/Scripts/CharacterController2D.cs
+++ Runner4Ever/Assets/Scripts/CharacterController2D.cs
@@ -4,6 +4,24 @@ using Lean.Touch;
 
 public class CharacterController2D : MonoBehaviour
 {
+	public enum Action
+	{
+		None,
+		Jump,
+		Accelerate,
+		Decelerate,
+		Dash,
+		Slide
+	}
+
+	public Action onTap;
+	public Action onSwipeLeft;
+	public Action onSwipeRight;
+	public Action onSwipeDown;
+	public Action onSwipeUp;
+	public Action onDoubleTap;
+
+
 	public float runSpeed = 0.1f;
 	public float jumpMagnitude = 0.1f;
 	private float upSpeed = 0;
@@ -31,33 +49,74 @@ public class CharacterController2D : MonoBehaviour
 
 	public void OnFingerTap(LeanFinger finger)
 	{
+		if(finger.TapCount == 1)
+			doAction(onTap);
+		else if(finger.TapCount == 2)
+			doAction(onDoubleTap);
+	}
+
+	public void doAction(Action action)
+	{
+		switch(action)
+		{
+			case Action.Jump : jump(); return;
+			case Action.Accelerate : accelerate(); return;
+			case Action.Decelerate : decelerate(); return;
+			case Action.Dash : dash(); return;
+			case Action.Slide : slide(); return;
+			default : return;
+		}
+	}
+
+	public void dash()
+	{
+
+	}
+
+	public void slide()
+	{
+
+	}
+
+	public void jump()
+	{
 		upSpeed = jumpMagnitude;
+	}
+
+	public void accelerate()
+	{
+		runSpeed *= 2;
+	}
+
+	public void decelerate()
+	{
+		runSpeed /= 2;
 	}
 
 	public void OnFingerSwipe(LeanFinger finger)
 	{		
-				// Store the swipe delta in a temp variable
-				var swipe = finger.SwipeScreenDelta;
+		// Store the swipe delta in a temp variable
+		var swipe = finger.SwipeScreenDelta;
 			
-				if (swipe.x < -Mathf.Abs(swipe.y)) // Left
-				{
-					runSpeed /= 2;
-				}
+		if (swipe.x < -Mathf.Abs(swipe.y)) // Left
+		{
+			doAction(onSwipeLeft);
+		}
 			
-				if (swipe.x > Mathf.Abs(swipe.y)) // Rigth
-				{
-					runSpeed *= 2;
-				}
+		if (swipe.x > Mathf.Abs(swipe.y)) // Rigth
+		{
+			doAction(onSwipeRight);
+		}
 			
-				if (swipe.y < -Mathf.Abs(swipe.x)) // Down
-				{
-					
-				}
+		if (swipe.y < -Mathf.Abs(swipe.x)) // Down
+		{
+			doAction(onSwipeDown);		
+		}
 			
-				if (swipe.y > Mathf.Abs(swipe.x)) // Up
-				{
-
-				}
+		if (swipe.y > Mathf.Abs(swipe.x)) // Up
+		{
+			doAction(onSwipeUp);
+		}
 	}
 
 }
